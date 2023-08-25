bc0ee7a2890f13c67cc81706e20c3ca90faff416
didntchooseaname <126179594+didntchooseaname@users.noreply.github.com>
2023-08-25 10:37:39 +0200

    tset

--- src/Application/UI/components/LoadingScreen.tsx
+++ src/Application/UI/components/LoadingScreen.tsx
@@ -161,20 +161,20 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                         <div style={styles.logoContainer}>
                             <div>
                                 <p style={styles.green}>
-                                    <b>Heffernan,</b>{' '}
+                                    <b>Hello there,</b>{' '}
                                 </p>
                                 <p style={styles.green}>
-                                    <b>Henry Inc.</b>
+                                    <b>gad to see you.</b>
                                 </p>
                             </div>
                         </div>
                         <div style={styles.headerInfo}>
                             <p>Released: 01/13/2000</p>
-                            <p>HHBIOS (C)2000 Heffernan Henry Inc.,</p>
+                            <p>HHBIOS (C)2000 contactit.fr,</p>
                         </div>
                     </div>
                     <div style={styles.body} className="loading-screen-body">
-                        <p>HSP S13 2000-2022 Special UC131S</p>
+                        <p>Guardia CS 2023-2025 - Cybersecurity expert</p>
                         <div style={styles.spacer} />
                         {showBiosInfo && (
                             <>
@@ -207,7 +207,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                             <p>
                                 All Content Loaded, launching{' '}
                                 <b style={styles.green}>
-                                    'Henry Heffernan Portfolio Showcase'
+                                    'Welcome to this presentation website'
                                 </b>{' '}
                                 V1.0
                             </p>
@@ -272,6 +272,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                     </div>
                 </div>
             </div>
+            /**
             {firefoxError && (
                 <div
                     style={Object.assign({}, styles.popupContainer, {
@@ -318,6 +319,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                     </div>
                 </div>
             )}
+            */
             {webGLError && (
                 <div
                     style={Object.assign({}, styles.popupContainer, {
